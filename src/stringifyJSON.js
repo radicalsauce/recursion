df6fd8d72467182e03e9d6c1020114a03716f39e
Kelly <kelly@groworganic.com>
2014-08-21 16:53:00 -0700

    Passes all test cases but objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,28 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return "null";  
+  } else if (typeof obj === "number") {
+    return ""+obj+"";
+  } else if (typeof obj === "string") {
+    return '"'+obj+'"';
+  } else if (typeof obj === "boolean") {
+    return ""+obj+"";
+  } else if (typeof obj === "undefined") {
+    return "undefined";
+  } else if (Array.isArray(obj)){
+    var arrayResult = [];
+    for (var i = 0; i < obj.length; i++){
+      arrayResult.push(stringifyJSON(obj[i]));
+    }
+    return "["+arrayResult+"]"; 
+  } else {
+    var objectResult = {};
+    for (var key in obj){
+      // this crap doesn't work yet
+    }
+    return "{"+objectResult+"}"; 
+  } 
 };
+
